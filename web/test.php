7f4b4e002ea191c4c25b447d5dfedfc2aa5ceddb
Sandip Mavani <mavanisandip94@gmail.com>
2017-09-10 01:17:09 +0530

    Update test.php

--- web/test.php
+++ web/test.php
@@ -1,8 +1,8 @@
 <?php
-print_r($_SERVER);
-print_r($_POST);
-print_r($_GET);
-print_r($_FILES);
+//print_r($_SERVER);
+//print_r($_POST);
+//print_r($_GET);
+//print_r($_FILES);
 file_put_contents("php://stderr", "sending push !!!".PHP_EOL);
 $json = [];
 $json['response'] = '<div class=\"contact_answer\"><h2>Thank you for contacting us. Your request will be reviewed shortly.</h2></div>';
