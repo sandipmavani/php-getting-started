d6e99d898d9c076d5649cfc61a92ccfe7b9efbbd
Sandip Mavani <mavanisandip94@gmail.com>
2017-09-29 18:33:46 +0530

    Update test.php

--- web/test.php
+++ web/test.php
@@ -4,6 +4,23 @@
 //print_r($_GET);
 //print_r($_FILES);
 file_put_contents("php://stderr", "sending push !!!".PHP_EOL);
+$string = "";
+$string .="Name :".$_GET['name']."                                         \n";
+$string .="Email :".$_GET['email']."                                         \n";
+$string .="Message :".$_GET['text']."                                         \n";
+$ch = curl_init("https://slack.com/api/chat.postMessage");
+$data = http_build_query([
+    "token" => "xoxp-182706105361-184074073622-185786601942-441527597eb23679a645c26abc5467d7",
+    "channel" => "inquiry", //"#mychannel",
+    "text" => $string, //"Hello, Foo-Bar channel message.",
+    "username" => "MySlackBot",
+]);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close($ch);
 $json = [];
 $json['response'] = '<div class=\"contact_answer\"><h2>Thank you for contacting us. Your request will be reviewed shortly.</h2></div>';
 header('Content-Type: application/json');
